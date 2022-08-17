15100970a5694ccc6dda402e489706c917f05630
isaac boateng <isaac.boateng@deltatre.com>
2022-08-17 22:48:02 +0100

    initial code

---
+++ src/appstore.notification/appstore.notification.api/Program.cs
@@ -0,0 +1,31 @@
+namespace appstore.notification.api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
